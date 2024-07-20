874b8830265aa7aa395758b354e1e1a8ad4f6613
Roshan Abhishek <roshanabhishek9595@gmail.com>
2024-07-20 16:54:16 +0530

    tic-tac-toe

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 import Auth from './pages/Auth';
 import PingPong from './pages/PingPong';
+import TicTacToe from './pages/TicTacToe';
 
 const App = () => {
   return (
     <Router>
       <div className="flex justify-center items-center h-screen bg-black">
         <nav className="absolute top-0 w-full flex justify-around p-4 bg-gray-800 text-white">
-          <Link to="/auth" className="hover:underline">Auth</Link>
           <Link to="/ping-pong" className="hover:underline">Ping Pong</Link>
+          <Link to="/tic-tac-toe" className="hover:underline">Tic Tac Toe</Link>
         </nav>
         <Routes>
           <Route path="/auth" element={<Auth />} />
+          <Route path="/tic-tac-toe" element={<TicTacToe />} />
           <Route path="/ping-pong" element={<PingPong />} />
         </Routes>
       </div>
---
+++ src/pages/TicTacToe.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TicTacToe = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-white text-3xl">Tic Tac Toe Page</h1>
+    </div>
+  );
+};
+
+export default TicTacToe;
